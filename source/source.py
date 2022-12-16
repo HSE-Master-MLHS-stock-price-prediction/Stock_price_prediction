9d1141eb4c1fdd47b02ea6b776cf7cc57f3f9a86
Evgeny Plyushch <eplyushch@ozon.ru>
2022-12-17 00:41:45 +0300

    small fixes

--- source/source.py
+++ source/source.py
@@ -10,14 +10,19 @@ class Config:
     RANDOM_STATE = 7575
 
 
-def _transform_to_DOIU(prices):
-    '''
+def _transform_to_DOIU(prices: np.ndarray | pd.DataFrame | pd.Series):
+    """
     Transform a timeseries of actual prices to the DOIU format.
 
     :param prices: prices timeseries to transform.
     :return:
-    '''
-    arr = pd.DataFrame(prices).pct_change().to_numpy()[1:]
+    """
+    if type(prices) is np.ndarray:
+        arr = pd.DataFrame(prices).pct_change().to_numpy()[1:]
+    elif (type(prices) is pd.DataFrame) or (type(prices) is pd.Series):
+        arr = prices.pct_change().to_numpy()[1:]
+    else:
+        raise ValueError(f"wrong type {type(prices)}")
     x = [1]
     for el in arr:
         x.append(x[-1] * (1 + el))
@@ -27,13 +32,13 @@ def _transform_to_DOIU(prices):
 def prepare_data(prices,
                  output_window_size=Config.OUTPUT_WINDOW_SIZE,
                  input_window_size=Config.INPUT_WINDOW_SIZE):
-    '''
+    """
     Create a list of windows possible to use to train/validate a model.
 
     :param prices: a whole timeseries of prices.
     :param output_window_size: the delay for which a prediction is made.
     :param input_window_size: the size of the train timeseries.
-    '''
+    """
     X = []
     y = []
     for i in range(0,
@@ -44,12 +49,19 @@ def prepare_data(prices,
         tr_x = _transform_to_DOIU(current_selection)
         y.append(tr_x[-1].reshape(1, -1))
         X.append(tr_x[:input_window_size].reshape(1, -1))
-
     X = np.concatenate(X, axis=0)
     y = np.concatenate(y, axis=0)
     return X, y
 
+
 if __name__ == '__main__':
     yndx = yf.Ticker("yndx")
-    prices = yndx.history(start="2011-05-24", end="2016-12-31", interval='1d').Close
-    print(pd.DataFrame(prices)[0])
+    prices = yndx.history(start="2011-05-24", end="2016-12-31",
+                          interval='1d').Close
+    # print(pd.DataFrame(prices))
+    X, y = prepare_data(prices)
+    print(X.shape, y.shape)
+    X, y = prepare_data(pd.DataFrame(prices))
+    print(X.shape, y.shape)
+    X, y = prepare_data(prices.to_numpy())
+    print(X.shape, y.shape)
